ab2a59f711294c7d1d432135756ef3091538d091
Pedro Gutierrez Jr <pedrogutie0@gmail.com>
2023-08-19 13:28:17 -0500

    removed sample data, persistence now integrated with UserDefaults

--- ViewModels/ListViewModel.swift
+++ ViewModels/ListViewModel.swift
@@ -13,19 +13,28 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    // save items any time items array is updated
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
+    // get items from user defaults
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "This is the second!", isCompleted: true),
-            ItemModel(title: "Third!", isCompleted: false)
-        ]
-        items.append(contentsOf: newItems)
+        // unwrap data
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     // delete list item
@@ -51,6 +60,13 @@ class ListViewModel: ObservableObject {
         // find index of item being updated in items array, then update isCompleted status
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    // save items with persistence
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
